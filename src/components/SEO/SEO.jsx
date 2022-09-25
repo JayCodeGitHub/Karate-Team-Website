36860ead63b41e721e5b71c0e75574b092829d00
Jakub Gryzinski <32836419+Jakubek5338@users.noreply.github.com>
2022-09-25 21:04:15 +0200

    added description meta tag

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -4,6 +4,10 @@ import Helmet from 'react-helmet';
 const SEO = () => (
   <Helmet>
     <html lang="pl" />
+    <meta
+      name="description"
+      content="Karate Team Oborniki od 2008 roku trenuje najlepszych zawodników w Polsce"
+    />
   </Helmet>
 );
 
